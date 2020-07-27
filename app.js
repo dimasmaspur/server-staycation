ebe3741d7959ffb7237b89f64ed5ac6fc7db2d93
dimasmaspur <dimasmaspur@gmail.com>
2020-07-27 23:40:41 +0700

    deploy to heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const flash = require('connect-flash');
 // import mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/db_staycation',{
+mongoose.connect('mongodb+srv://dimasmaspur:bwamern@cluster0.sx8sf.mongodb.net/db_staycation?retryWrites=true&w=majority',{
   useNewUrlParser:true,
   useUnifiedTopology:true,
   useCreateIndex:true,
